24cbc4773080f887710422e0710f313b11c4f943
marcelom97 <marcelomollaj@gmail.com>
2021-02-22 20:33:02 +0200

    feat: added virtual fields and post shcema

---
+++ users/test/subdocument_test.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+
+const User = require('../src/user');
+
+describe('Subdocuments', () => {
+  it('an create a subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'Post title' }],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'Post title');
+        done();
+      });
+  });
+
+  it('can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 1);
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
+  });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
+});

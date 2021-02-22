8993645ded83fd93188501f16556ca488bc1adc2
marcelom97 <marcelomollaj@gmail.com>
2021-02-22 21:08:00 +0200

    feat: association and deep population

--- users/src/user.js
+++ users/src/user.js
@@ -14,6 +14,12 @@ const UserSchema = new Schema({
   },
   posts: [PostSchema],
   likes: Number,
+  blogPosts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'blogPost',
+    },
+  ],
 });
 
 UserSchema.virtual('postCount').get(function () {

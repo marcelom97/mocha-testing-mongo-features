c5bddf28c1fdccfa5ebe9a5cf33062a1ca7e4d7b
marcelom97 <marcelomollaj@gmail.com>
2021-02-23 17:40:27 +0200

    refactor: added pre remove middleware

--- users/src/user.js
+++ users/src/user.js
@@ -26,6 +26,13 @@ UserSchema.virtual('postCount').get(function () {
   return this.posts.length;
 });
 
+UserSchema.pre('remove', function (next) {
+  const BlogPost = mongoose.model('blogPost');
+
+  // When removing a user also remove all his blogposts
+  BlogPost.remove({ _id: { $in: this.blogPosts } }).then(() => next());
+});
+
 const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
